171c233883ce9253e61e4b31a9dac88b90439339
Emil Ötting <emil.oetting@gmx.de>
2024-12-02 11:28:24 +0100

    augmented test_fairsharer

--- test/test_fairsharer.py
+++ test/test_fairsharer.py
@@ -44,10 +44,22 @@ def test_fairsharer():
     assert fs.fair_sharer(values=matrix_2, num_iterations=1, share=0.1) == [0, 100, 800, 100, 800, 0, 0, 0]
     assert fs.fair_sharer(values=matrix_2, num_iterations=2, share=0.1) == [0, 180, 620, 180, 800, 0, 0, 0]
 
+    iterations = np.int64(2)
+    assert fs.fair_sharer(values=[0, 1000, 800, 0], num_iterations=iterations, share=0.1) == [100, 890, 720, 90]
+
     with pytest.raises(TypeError) as err_info_2:
-        fs.fair_sharer(values=[1,2,3], num_iterations="2", share=0.1)
+        fs.fair_sharer(values=[0, 1000, 800, 0], num_iterations="2", share=0.1)
     assert str(err_info_2.value) == "Object of unsupported type <class 'str'>. Expected <class 'int'>."
 
     with pytest.raises(TypeError) as err_info_3:
-        fs.fair_sharer(values=[1,2,3], num_iterations=2, share=["0.1"])
+        fs.fair_sharer(values=[0, 1000, 800, 0], num_iterations=2, share=["0.1"])
     assert str(err_info_3.value) == "Object of unsupported type <class 'list'>. Expected <class 'float'>."
+
+    iterations = "random_string"
+    with pytest.raises(TypeError) as err_info_4:
+        fs.fair_sharer(values=[0, 1000, 800, 0], num_iterations=iterations, share=0.1)
+    assert str(err_info_4.value) == "Object of unsupported type <class 'str'>. Expected <class 'int'>."
+
+    with pytest.raises(TypeError) as err_info_4:
+        fs.fair_sharer(values=[0, 1000, 800, 0], num_iterations=0.5, share=0.1)
+    assert str(err_info_4.value) == "Object of unsupported type <class 'float'>. Expected <class 'int'>."
